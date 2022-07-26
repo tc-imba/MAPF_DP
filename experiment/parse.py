0824069eb6351b177502b60f048aa3496234c052
tc-imba <liuyh970615@gmail.com>
2022-07-27 01:34:30 +0800

    test

--- experiment/parse.py
+++ experiment/parse.py
@@ -16,6 +16,7 @@ obstacles_list = [90, 180, 270]
 agents_list = [10, 20, 30]
 simulators_list = ["online", "default"]
 delay_ratios_list = [0.01, 0.05, 0.1, 0.2]
+delay_types_list = ["agent", "edge"]
 
 
 def parse_data(data_type) -> pandas.DataFrame:
@@ -35,85 +36,87 @@ def parse_data(data_type) -> pandas.DataFrame:
         'feasibility_1', 'feasibility_2', 'feasibility_3', 'feasibility_4',
     ]
     column_names = [
-        "simulator", "agents", "timestep", "interval", "rate",
+        "simulator", "agents", "timestep", "interval", "rate", "delay_type",
         "value", "time", "feasibility", "cycle", "execution_time", "first_agent_arriving",
         "cycle_count", "cycle_agents", "unblocked_agents", "feasibility_count",
         "feasibility_type_a", "feasibility_type_b", "feasibility_type_c",
     ]
 
     main_df = pandas.DataFrame(columns=column_names)
-    for obstacles in obstacles_list:
-        for simulator in simulators_list:
-            for agents in agents_list:
-                for start in starts_list:
-                    for interval in intervals_list:
-                        for rate in delay_ratios_list:
-                            for feasibility, cycle in [("h", "h"), ("h", "n"), ("n", "h"), ("n", "n"), ("n", "o"),
-                                                       ("h", "o")]:
-                                # for feasibility, cycle in [("h", "h"), ("n", "h")]:
-                                file = f"{simulator}-{obstacles}-{agents}-edge-{rate}-{start}-{interval}-{feasibility}-{cycle}.csv"
-                                try:
-                                    df = pandas.read_csv(os.path.join(result_dir, file), header=None,
-                                                         names=header_names)
-                                    df.sort_values(by=['map', 'agent', 'iteration'], inplace=True)
-                                    df.to_csv(os.path.join(parsed_result_dir, file), index=False)
+    for delay_type in delay_types_list:
+        for obstacles in obstacles_list:
+            for simulator in simulators_list:
+                for agents in agents_list:
+                    for start in starts_list:
+                        for interval in intervals_list:
+                            for rate in delay_ratios_list:
+                                for feasibility, cycle in [("h", "h"), ("h", "n"), ("n", "h"), ("n", "n"), ("n", "o"),
+                                                           ("h", "o")]:
+                                    # for feasibility, cycle in [("h", "h"), ("n", "h")]:
+                                    file = f"{simulator}-{obstacles}-{agents}-{delay_type}-{rate}-{start}-{interval}-{feasibility}-{cycle}.csv"
+                                    try:
+                                        df = pandas.read_csv(os.path.join(result_dir, file), header=None,
+                                                             names=header_names)
+                                        df.sort_values(by=['map', 'agent', 'iteration'], inplace=True)
+                                        df.to_csv(os.path.join(parsed_result_dir, file), index=False)
 
-                                    value = npy.mean(df['value'])
-                                    time = npy.mean(df['time'])
-                                    execution_time = 0
-                                    first_agent_arriving = 0
-                                    cycle_count = 0
-                                    cycle_agents = 0
-                                    unblocked_agents = 0
-                                    feasibility_count = 0
-                                    feasibility_type_a = 0
-                                    feasibility_type_b = 0
-                                    feasibility_type_c = 0
-                                    if len(df.columns) > 8:
-                                        execution_time = npy.mean(df['execution_time'])
-                                        first_agent_arriving = npy.mean(df['first_agent_arriving'])
-                                        cycle_count = npy.mean(df['cycle_count'])
-                                        cycle_agents = npy.mean(df['cycle_agents'])
-                                        unblocked_agents = npy.mean(df['unblocked_agents'])
-                                        feasibility_count = npy.mean(df['feasibility_count'])
-                                    if len(df.columns) > 12:
-                                        feasibility_count_all = npy.sum(
-                                            df[['feasibility_1', 'feasibility_2', 'feasibility_3', 'feasibility_4']],
-                                            axis=1
-                                        )
-                                        feasibility_type_a = npy.nanmean(df['feasibility_1'] / feasibility_count_all)
-                                        feasibility_type_b = npy.nanmean(df['feasibility_4'] / feasibility_count_all)
-                                        feasibility_type_c = npy.nanmean(
-                                            (df['feasibility_2'] + df['feasibility_3']) / feasibility_count_all)
-                                except:
-                                    value = 0
-                                    time = 0
-                                # print(file, mean)
-                                if time == 0:
-                                    continue
-                                row = {
-                                    "simulator": simulator,
-                                    "obstacles": obstacles,
-                                    "agents": agents,
-                                    "timestep": start,
-                                    "interval": interval,
-                                    "rate": rate,
-                                    "value": value,
-                                    "time": time,
-                                    "feasibility": feasibility == "n" and "exhaustive" or "heuristic",
-                                    "cycle": cycle == "n" and "semi-naive" or (
-                                            cycle == "o" and "naive" or "proposed"),
-                                    "execution_time": execution_time,
-                                    "first_agent_arriving": first_agent_arriving,
-                                    "cycle_count": cycle_count,
-                                    "cycle_agents": cycle_agents,
-                                    "unblocked_agents": unblocked_agents,
-                                    "feasibility_count": feasibility_count,
-                                    "feasibility_type_a": feasibility_type_a,
-                                    "feasibility_type_b": feasibility_type_b,
-                                    "feasibility_type_c": feasibility_type_c,
-                                }
-                                main_df = main_df.append(row, ignore_index=True)
+                                        value = npy.mean(df['value'])
+                                        time = npy.mean(df['time'])
+                                        execution_time = 0
+                                        first_agent_arriving = 0
+                                        cycle_count = 0
+                                        cycle_agents = 0
+                                        unblocked_agents = 0
+                                        feasibility_count = 0
+                                        feasibility_type_a = 0
+                                        feasibility_type_b = 0
+                                        feasibility_type_c = 0
+                                        if len(df.columns) > 8:
+                                            execution_time = npy.mean(df['execution_time'])
+                                            first_agent_arriving = npy.mean(df['first_agent_arriving'])
+                                            cycle_count = npy.mean(df['cycle_count'])
+                                            cycle_agents = npy.mean(df['cycle_agents'])
+                                            unblocked_agents = npy.mean(df['unblocked_agents'])
+                                            feasibility_count = npy.mean(df['feasibility_count'])
+                                        if len(df.columns) > 12:
+                                            feasibility_count_all = npy.sum(
+                                                df[['feasibility_1', 'feasibility_2', 'feasibility_3', 'feasibility_4']],
+                                                axis=1
+                                            )
+                                            feasibility_type_a = npy.nanmean(df['feasibility_1'] / feasibility_count_all)
+                                            feasibility_type_b = npy.nanmean(df['feasibility_4'] / feasibility_count_all)
+                                            feasibility_type_c = npy.nanmean(
+                                                (df['feasibility_2'] + df['feasibility_3']) / feasibility_count_all)
+                                    except:
+                                        value = 0
+                                        time = 0
+                                    # print(file, mean)
+                                    if time == 0:
+                                        continue
+                                    row = {
+                                        "simulator": simulator,
+                                        "obstacles": obstacles,
+                                        "agents": agents,
+                                        "timestep": start,
+                                        "interval": interval,
+                                        "rate": rate,
+                                        "delay_type": delay_type,
+                                        "value": value,
+                                        "time": time,
+                                        "feasibility": feasibility == "n" and "exhaustive" or "heuristic",
+                                        "cycle": cycle == "n" and "semi-naive" or (
+                                                cycle == "o" and "naive" or "proposed"),
+                                        "execution_time": execution_time,
+                                        "first_agent_arriving": first_agent_arriving,
+                                        "cycle_count": cycle_count,
+                                        "cycle_agents": cycle_agents,
+                                        "unblocked_agents": unblocked_agents,
+                                        "feasibility_count": feasibility_count,
+                                        "feasibility_type_a": feasibility_type_a,
+                                        "feasibility_type_b": feasibility_type_b,
+                                        "feasibility_type_c": feasibility_type_c,
+                                    }
+                                    main_df = main_df.append(row, ignore_index=True)
     return main_df
 
 
