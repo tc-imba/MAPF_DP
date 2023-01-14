6abb1cd776015d5be93c76f1215a7fd1a5f453e6
tc-imba <liuyh970615@gmail.com>
2023-01-15 02:30:52 +0800

    feat: update parse.py

--- experiment/parse.py
+++ experiment/parse.py
@@ -92,7 +92,7 @@ def parse_data(result_dir, data_type, category) -> pandas.DataFrame:
                                 base_df = base_df[['map', 'agent', 'iteration']]
 
                                 for simulator in simulators_list:
-                                    if simulator == "default" and (feasibility != "h" or cycle != "h"):
+                                    if (simulator == "default" or simulator == "replan") and (feasibility != "h" or cycle != "h"):
                                         continue
 
                                     df = raw_dfs[simulator]
