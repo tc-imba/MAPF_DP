dc57b9838ff214f77fa238c0f8480004bf9583d6
tc-imba <liuyh970615@gmail.com>
2023-05-19 17:31:07 +0800

    feat: test parameters

--- experiment/app.py
+++ experiment/app.py
@@ -17,13 +17,13 @@ class AppArguments:
 
 
 def experiment_base_options(f):
-    @click.option("--obstacles", type=str, default="90,270", callback=validate_list(int))
-    @click.option("--agents", type=str, default="10,20,30", callback=validate_list(int))
-    @click.option("--simulators", type=str, default="online,default,replan,prioritized,prioritized_opt,pibt", callback=validate_list(str))
+    @click.option("--obstacles", type=str, default="90,180,270", callback=validate_list(int))
+    @click.option("--agents", type=str, default="30", callback=validate_list(int))
+    @click.option("--simulators", type=str, default="online,default,replan,pibt", callback=validate_list(str))
     @click.option("--k-neighbors", type=str, default="2", callback=validate_list(int))
     @click.option("--delay-types", type=str, default="agent", callback=validate_list(str))
-    @click.option("--delay-ratios", type=str, default="0.1,0.2,0.3", callback=validate_list(float))
-    @click.option("--delay-intervals", type=str, default="1,10,20", callback=validate_list(int))
+    @click.option("--delay-ratios", type=str, default="0.1", callback=validate_list(float))
+    @click.option("--delay-intervals", type=str, default="0,1,10,20", callback=validate_list(int))
     @click.pass_context
     @wraps(f)
     def wrapper(ctx, obstacles, agents, simulators, k_neighbors, delay_types, delay_ratios, delay_intervals,
